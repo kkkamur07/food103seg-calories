1e6c9977e078176ce3b8a8a5526c7354137322c1
Akshata Lokhande <akshatalokhande03@gmail.com>
2025-07-05 21:16:34 +0200

    apifile

--- src/tests/integrationtests/test_apis.py
+++ src/tests/integrationtests/test_apis.py
@@ -1,3 +1,3 @@
-from fastapi.testclient import TestClient
-from <project-name>.api import app
-client = TestClient(app)
+#from fastapi.testclient import TestClient
+#from <project-name>.api import app
+#client = TestClient(app)
