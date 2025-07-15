db5d13d30c8b21cc8eb4806865b6d198a33aca2c
Akshata-Lokhande <akshatalokhande02@gmail.com>
2025-07-15 13:17:59 +0200

    updates the test file for data

--- src/tests/test_data1.py
+++ src/tests/test_data1.py
@@ -6,10 +6,10 @@ import numpy as np
 from unittest.mock import MagicMock, patch, mock_open
 from PIL import Image
 
-from src.segmentation.data import FoodSegDataset,data_loaders
+from segmentation.data import FoodSegDataset,data_loaders
 
 @pytest.fixture()
-#Set up temporary directory with dummy image and annotation files.
+#Dummy data
 def dummy_data(tmp_path):
     base_dir=tmp_path/"dummy_data"
     img_root_dir=base_dir/"data"/"Images"/"img_dir"
@@ -27,7 +27,7 @@ def dummy_data(tmp_path):
 
     return str(base_dir)
 
-# To check if FoodSegDataset reflects correct number of dummy files created and output types 
+
 def test_len_matches_filecount(dummy_data):
     ann_transform=transforms.Compose(
         [
@@ -42,11 +42,11 @@ def test_len_matches_filecount(dummy_data):
     img_transform = transforms.Compose(
         [
             transforms.Resize((224, 224)),
-            transforms.ToTensor(),  # converts PIL to float tensor and scales to [0, 1]
+            transforms.ToTensor(),  
         ]
     )
 
-#Create a dataset instance to list the actual dummy files
+#Dataset Instance
     train_dataset=FoodSegDataset(
         base_dir=dummy_data,
         mode="train",
@@ -54,50 +54,38 @@ def test_len_matches_filecount(dummy_data):
         ann_transform=ann_transform
     )
 
-    #test if __len__ reflects number of dummy files=2
-    assert len(train_dataset)==2
-    
-    #test if __getitem__ 
+    assert len(train_dataset)==2     
     image,mask=train_dataset[0]
 
-    #assert output types and shapes after actual transforms
+
     assert isinstance(image,torch.Tensor)
+
     assert image.shape==(3,224,224)
     assert isinstance(mask,torch.Tensor)
     assert mask.shape==(224,224)
     assert mask.dtype==torch.long
 
-    #Assert labels are within expected range
     assert torch.all(mask>=0) and torch.all(mask<=103)
 
 
 def test_data_loaders_batch(dummy_data):
     train_loader, test_loader = data_loaders(dummy_data, batch_size=2, num_workers=0)
-   
-   
-    #Checks setup
 
-    # check if returned objects are instances of the actual classes
     assert isinstance(train_loader, torch.utils.data.DataLoader)
     assert isinstance(test_loader, torch.utils.data.DataLoader)
 
-    # Assert that the datasets within the loaders are instances of FoodSegDataset
     assert isinstance(train_loader.dataset, FoodSegDataset)
     assert isinstance(test_loader.dataset, FoodSegDataset)
-
-    # Assert  shuffle settings
+    
     assert train_loader.batch_size == 2
     assert train_loader.num_workers == 0
-
+    
     assert test_loader.batch_size == 2
     assert test_loader.num_workers == 0
-
-    #Checks data flow
-
-    # Assert dataset sizes are correct based on the dummy data
+    
     assert len(train_loader.dataset) == 2
     assert len(test_loader.dataset) == 2 
-    # Check if loader yield one batch of two samples
+
     x_train, y_train = next(iter(train_loader))
     x_test,  y_test  = next(iter(test_loader))
 
